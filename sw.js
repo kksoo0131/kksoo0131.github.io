fb63e14176e2d0072cbb4660c9f260ab285b1115
kksoo0131 <a67636521@gmail.com>
2023-06-26 16:26:11 +0900

    test

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230626.161209';
+const cacheName = 'chirpy-20230626.162457';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
