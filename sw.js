ad8eed0fb9f146e345e263cc7a8fe6475b5688be
kksoo0131 <a67636521@gmail.com>
2023-06-26 16:58:03 +0900

    first post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230626.162457';
+const cacheName = 'chirpy-20230626.165704';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
